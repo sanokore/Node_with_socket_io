cd35f5494bf7b25665c6cc792d5ad01fe55dfac0
satish <satishekele@yahoo.com>
2014-10-30 17:47:43 +0530

    Added dynamic port logic for heroku

--- server.js
+++ server.js
@@ -50,8 +50,8 @@ var server = http.createServer(function(request, response) {
   serveStatic(response, cache, absPath);
 });
 
-server.listen(3000, function() {
-  console.log("Server listening on port 3000.");
+server.listen(process.env.PORT || 5000, function() {
+  console.log("Server listening on port 5000.");
 });
 
 var chatServer = require('./lib/chat_server');
